cc52f37805dce9b80ba7ca41b1a051277accd5fe
phamlethaohien <phamlethaohien.github.com>
2023-02-18 22:30:08 +0700

    Lay ten trong ho va ten

---
+++ String/STACHTEN.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    stringstream ss(s);
+
+    string token;
+
+    while (ss >> token) {
+        continue;
+    }
+
+    cout << token;
+
+}
