9831cc925657e0258670794fa618cf73c107a414
phamlethaohien <phamlethaohien.github.com>
2023-09-18 05:52:05 +0700

    DPLCS Max number

--- Dynamic Programming/DPLCSMA.cpp
+++ Dynamic Programming/DPLCSMA.cpp
@@ -1,35 +1,53 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void solve(string a, string b) {
-    int dp[a.size() + 1][b.size() + 1];
-    memset(dp, 0, sizeof(dp));
+const int mxn = 1005;
 
-    for (int i = 1; i <= a.size(); i++) {
-        for (int j = 1; j <= b.size(); j++) {
-            if (a[i - 1] == b[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
-            else dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+string dp[mxn][mxn] = {};
+
+void solve(string a, string b, int n, int m) {
+    a = ' ' + a;
+    b = ' ' + b;
+
+    bool check = false;
+    if (a.find('0') != -1 && b.find('0') != -1) check = true;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (a[i] == b[j]) {
+                if (a[i] != '0' || dp[i - 1][j - 1] != "") {
+                    dp[i][j] = dp[i - 1][j - 1] + a[i];
+                    check = true;
+                }
+            }
+            else {
+                if (dp[i - 1][j].size() == dp[i][j - 1].size())
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                else {
+                    if (dp[i - 1][j].size() > dp[i][j - 1].size())
+                        dp[i][j] = dp[i - 1][j];
+                    else
+                        dp[i][j] = dp[i][j - 1];
+                }
+            }
         }
     }
-    int idx = dp[a.size()][b.size()];
-
-    vector<char> v(idx, '0');
-    int i = a.size(), j = b.size();
-    while (i > 0 && j > 0) {
-        if (a[i - 1] == b[j - 1]) {
-            v[idx - 1] = a[i - 1];
-            i--; j--; idx--;
-        }
-        else if (dp[i - 1][j] > dp[i][j - 1]) i--;
-        else j--;
+    if (dp[n][m] == "" && check) {
+        cout << 1 << "\n";
+        cout << "LCS MAX NUMBER is 0";
     }
-    for (auto it : v) cout << it;
+    else if (!check) {
+        cout << 0 << "\n";
+        cout << "LCS MAX NUMBER is NULL";
+    }
+    else {
+        cout << dp[n][m].size() << "\n";
+        cout << "LCS MAX NUMBER is " << dp[n][m];
+    }
 }
 
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
     string a, b; cin >> a >> b;
-    solve(a, b);
+    solve(a, b, a.size(), b.size());
 }
-
