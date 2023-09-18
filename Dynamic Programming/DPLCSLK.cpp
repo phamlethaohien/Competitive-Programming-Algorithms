ea913b7368df88463f5a0db84d48b4db57d16c5a
phamlethaohien <phamlethaohien.github.com>
2023-09-18 08:52:30 +0700

    DPLCSLK

--- Dynamic Programming/DPLCSLK.cpp
+++ Dynamic Programming/DPLCSLK.cpp
@@ -1,26 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int m, n, a[1001], b[1001];
+int m, n, a[1003], b[1003];
+int dp[1003][1003];
 
 void solve(int a[], int b[], int m, int n) {
-    int dp[m + 2][n + 2];
-    dp[-1][-1] = 0;
-    for (int i = 0; i <= m; i++) {
-        for (int j = 0; j <= n; j++) {
-            if (i == 0 || j == 0) {
-                dp[i][j] = 0;
-            }
-            else if (a[i - 1] == b[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-                if (a[i] == b[i]) dp[i][j] = dp[i][j] - 1;
-            }
-            else if (a[i - 1] != b[j - 1]) {
+    memset(dp, 0, sizeof(dp));
+    for (int i = 2; i <= m + 1; i++) {
+        for (int j = 2; j <= n + 1; j++) {
+            if (a[i - 2] == b[j - 2])
+                dp[i][j] = dp[i - 2][j - 2] + 1;
+            else
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-            }
         }
     }
-    cout << dp[m][n] << "\n";
+    cout << dp[m + 1][n + 1] << "\n";
 }
 
 int main() {
